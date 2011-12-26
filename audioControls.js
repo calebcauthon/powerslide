7b4edc99b7073bbbae69a97ec0ade9871c6c5c9b
Caleb Cauthon <caleb@caleb-PC.(none)>
2011-12-26 16:05:18 -0600

    Added audio controls

--- audioControls.js
+++ audioControls.js
@@ -1,5 +1,46 @@
 function AudioControls() {
-	return {
+	
+	var isFlash = false;
+	var load = function(mp3_file) {				
+		if(swfobject.getFlashPlayerVersion().major > 0)
+		{
+			isFlash = true;
+			$f("player", "http://releases.flowplayer.org/swf/flowplayer-3.2.7.swf", {
+				clip: {
+					url: mp3_file,
+				}
+			});
+		}
+		else
+		{
+			$('#html5player')[0].setAttribute('src', mp3_file);
+			$('#html5player')[0].play();						
+		}
 		
+	};
+	var play = function() {		
+		if(isFlash)
+			$f().play();
+		else
+			$('#html5player')[0].play();		
+	};
+	var pause = function() {
+		if(isFlash)
+			$f().pause();
+		else
+			$('#html5player')[0].pause();		
+	};
+	var seek = function(sec) {
+		if(isFlash)
+			$f().seek(sec);
+		else
+			$('#html5player')[0].currentTime = sec;		
+	};
+	
+	return {
+		load: load,
+		play: play,
+		pause: pause,
+		seek: seek
 	};
 };
---
+++ audioControls_spec.js
@@ -0,0 +1,144 @@
+describe('AudioControls', function() {
+	
+	var adjustment;	
+	var PLAYING = 3;
+	var PAUSED = 4;
+	var lastTime;
+	var getState = function() {
+		if(swfobject.getFlashPlayerVersion().major > 0)
+			return $f().getState(); // flowplayer
+		else
+		{	
+			var t = $('#html5player')[0].currentTime;
+			
+			if(t > 0 && t == lastTime)
+				var r = PAUSED; // paused
+			else if(t > 0 && t > lastTime && (t - lastTime) < .05)
+			{
+				var r = PLAYING; // playing
+			}
+				
+			lastTime = t;
+			
+			return r;
+			
+			
+		}
+	};
+	var getTime = function() {
+		if(swfobject.getFlashPlayerVersion().major > 0)
+			return $f().getTime(); // flowplayer
+		else
+			return $('#html5player')[0].currentTime;		
+	};
+	//setInterval(function() { console.log(getTime()); }, 100);
+	var audioControls;
+	beforeEach(function() {
+		audioControls = new AudioControls();
+	});
+	afterEach(function() {
+		$('#player').html('');
+		$('#audioWrapper').html('').html('<audio id="html5player" controls="controls">');
+	});	
+	it('should exist', function() {
+		var audioControls = new AudioControls();
+	});
+	describe('load method', function() {
+		it('should have a load method', function() {
+			audioControls.load('http://releases.flowplayer.org/data/fake_empire.mp3');
+			waitsFor(function() {			
+				return (getState() == 3); // playing				
+			}, "flowplayer failed to load", 5000);
+		});		
+	});
+	describe('pause method', function() {
+		it('should pause the track', function() {
+			audioControls.load('http://releases.flowplayer.org/data/fake_empire.mp3');			
+			waitsFor(function() {						
+				return (getState() == 3); // playing				
+			}, "flowplayer failed to load", 2000);
+			
+			runs(function() {							
+				audioControls.pause();				
+			});
+			waitsFor(function() {
+				return getState() == 4; // paused
+			}, "", 5000);
+		});
+	});
+	describe('play method', function() {
+		it('calculate playtime adjustment', function() {
+			audioControls.load('http://releases.flowplayer.org/data/fake_empire.mp3');			
+			waitsFor(function() {						
+				return (getState() == 3); // playing				
+			}, "flowplayer failed to load", 2000);
+
+			runs(function() {							
+				audioControls.pause();												
+				audioControls.seek(6);
+				audioControls.play();				
+				var d = new Date();
+				t1 = d.getTime();
+			});
+			
+			waitsFor(function() {				
+				return getState() == 3; // playing							
+			}, "", 5000);	
+
+			runs(function() {
+				var d = new Date();
+				t2 = d.getTime();
+				adjustment = t2 - t1;
+			});			
+		});
+		it('should play the track', function() {
+			audioControls.load('http://releases.flowplayer.org/data/fake_empire.mp3');			
+			waitsFor(function() {						
+				return (getState() == 3); // playing				
+			}, "flowplayer failed to load", 2000);
+			
+			runs(function() {							
+				audioControls.pause();				
+				audioControls.play();
+			});
+			waitsFor(function() {
+				return getState() == 3; // paused
+			}, "", 5000);
+		});
+		it('should play the track immediately', function() {
+			audioControls.load('http://releases.flowplayer.org/data/fake_empire.mp3');			
+			waitsFor(function() {						
+				return (getState() == 3); // playing				
+			}, "flowplayer failed to load", 2000);
+			
+			runs(function() {							
+				audioControls.pause();												
+				audioControls.seek(6);
+				audioControls.play();				
+				expect(getTime()).toBeGreaterThan(5.9);				
+			});
+			
+			waits(adjustment);
+			waits(1200);		
+			
+			runs(function() {						
+				expect(getTime()).toBeGreaterThan(7.2);				
+			});
+		});
+	});
+	describe('seek method', function() {
+		it('should seek to the current spot while playing', function() {
+			audioControls.load('http://releases.flowplayer.org/data/fake_empire.mp3');
+			waitsFor(function() {			
+				return (getState() == 3); // playing				
+			}, "flowplayer failed to load", 2000);
+			runs(function() {
+				audioControls.play();
+				audioControls.seek(10);
+				audioControls.pause();
+				expect(getTime()).toBeGreaterThan(9.9);
+				expect(getTime()).toBeLessThan(10.1);
+			});
+		});		
+	});	
+});
